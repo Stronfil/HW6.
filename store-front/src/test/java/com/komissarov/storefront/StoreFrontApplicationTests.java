c6edac3a6517552f1852298165bdec0ae42b888d
Stronfil <tambovskii99@gmail.com>
2021-08-10 21:25:25 +0300

    Initial commit

---
+++ store-front/src/test/java/com/komissarov/storefront/StoreFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komissarov.storefront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
